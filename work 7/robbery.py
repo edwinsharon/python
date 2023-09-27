317775e98dd2f70cea2328d42c0210ac2624f155
edwinsharon <edwinachayan@gmail.com>
2023-09-27 13:36:27 +0530

    done

---
+++ work 7/robbery.py
@@ -0,0 +1,37 @@
+# mylist=[7,12,14,8,4]
+# list_len=len(mylist)
+# max_element=max(mylist)
+# max_element_index=mylist.index(max_element)
+# sum=0
+# # for i in range(0,5):
+# #     max_element=max(mylist)
+# #     second_max=mylist.index(max_element)
+# #     print('e is',second_max)
+# #     print()
+# #     if ( second_max==max_element_index-1 or second_max==max_element_index+1):
+# #         break 
+# #     else:
+# #       sum=sum+max_element
+# #     max_element_index=mylist.index(max_element)
+# #     # mylist.remove(max_element)  
+# p
+# print(mylist)
+# print(sum)
+
+
+
+
+
+
+# # for i in range(5):
+# #     for j in range 
+mylist=[7,12,14,8,4]
+for i in range(0,5,2):
+    b=mylist[i]+mylist[i+2]
+    c=mylist[i+2]+mylist[i+4]
+if(b>c):
+    print(b)
+else:
+    print(c)
+         
+
