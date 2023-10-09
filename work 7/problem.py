eac62d6ef20ba0ef145e674f4b2ab588743c89b7
edwinsharon <edwinachayan@gmail.com>
2023-10-09 13:19:47 +0530

    done

--- work 7/problem.py
+++ work 7/problem.py
@@ -1,16 +1,16 @@
-# nums=[2,7,11,15]
-# target=9
-# for i in range(len(nums)):
-#     for k in range(len(nums)):
-#         t=nums[i]+nums[k]
-#         if t==target:
-#             print(i,k)
+nums=[2,7,11,15]
+target=9
+for i in range(len(nums)):
+    for k in range(len(nums)):
+        t=nums[i]+nums[k]
+        if t==target:
+            print(i,k)
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []  # No solution found
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+#         for i in range(n - 1):
+#             for j in range(i + 1, n):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return []  # No solution found
---
+++ work 7/problem4.py
@@ -0,0 +1,7 @@
+# ist=l["car","cab","call"]
+# a=len(list)
+# for i in range(a):
+    
+a=input("enter a name")
+for i in range(len(a)):
+    print("*",'end()')
