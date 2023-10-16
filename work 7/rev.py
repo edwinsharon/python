801c36a8d14d3ed9b8a76b1f69513ed040dec687
edwinsharon <edwinachayan@gmail.com>
2023-10-16 13:09:29 +0530

    done

---
+++ work 7/rev.py
@@ -0,0 +1,84 @@
+# my_list=[1,2.3,'sad',True,[1,4,7]]
+
+# numbers=[1,2,3,4,5,6,7]
+# sub_list=numbers[1:5]
+# print(sub_list)
+
+# list1=[1,2,3]
+# list2=[4,5,6]
+# combined_list=[list1+list2]
+# print(combined_list)
+
+# fruits=['apple','banana','cherry']
+# fruits.append('kiwi')
+# print(fruits)
+# fruits.insert(1,"orange")
+# print(fruits)
+# fruits.append('grape')
+# fruits.append('blueberry')
+# print(fruits)
+# fruits.remove("banana")
+# print(fruits)
+
+
+# numbers=[5,2,8,1,3]
+# numbers.sort()
+# a=numbers
+# print(a)
+# numbers.sort(reverse=True)
+# b=numbers
+# print(b)
+
+
+# my_list=[1,2.3,'sad',True,[1,4,7]]
+# print(my_list[-1])
+
+
+# fruits=['apple','banana','cherry']
+# fruits.append('kiwi')
+# print(fruits)
+# fruits.insert(1,"orange")
+# print(fruits)
+# fruits.append('grape')
+# fruits.append('blueberry')
+# print(fruits)
+# fruits.remove("banana")
+# print(fruits)
+# x = fruits.count("apple")
+# print(x)
+
+# num=[5,2,8,1,3]
+# a=num[::-1]
+# print(a)
+
+# original_list=[1,2,3]
+# copied_list=original_list.copy()
+# print(copied_list)
+# copied_list.append(4)
+# print(copied_list)
+# print(original_list)
+
+
+
+
+# numbers = [5,2,8,1,3] 
+# result = map(, numbers) 
+# print(list(result)) 
+
+
+# numbers = [5,2,8,1,3] 
+# print(max(numbers))
+# print(min(numbers))
+
+# fruits=['apple','banana','cherry']
+# print('banana' in fruits)
+
+
+# fruits=['apple','banana','cherry']
+# print(len(fruits))
+
+
+fruits=['apple','banana','cherry']
+fruits.remove("banana")
+fruits.insert(1,'grapefruit')
+print(fruits)
