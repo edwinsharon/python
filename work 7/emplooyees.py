64cdabb84664d04ad8aec51b5aa73fc30a45b528
edwinsharon <edwinachayan@gmail.com>
2023-10-28 12:58:10 +0530

    done

--- work 7/emplooyees.py
+++ work 7/emplooyees.py
@@ -2,6 +2,7 @@ choice='y'
 bank=[]
 new_customer={}
 acc_no=0
+balance=0
 while choice=='y'or choice=='Y':
     print("""
 1)add customer
@@ -17,30 +18,35 @@ while choice=='y'or choice=='Y':
         age=int(input("enter the age"))    
         new_customer["age"]=age
         contact_number=int(input("enter the phone number"))
-        new_customer["conatct"]=contact_number
+        new_customer["contact"]=contact_number
         new_customer["acc_no"]=acc_no + 1
-        new_customer["balance"]=0
+        new_customer["balance"]=balance
         print(new_customer)
         bank.append(new_customer.copy()) 
     elif option==2:
         print(bank)
     elif option==3:
-     choice1=input("""
+     choice1=int(input("""
 1)deposit
 2)withdraw
-enter your choice                          
+ enter your choice
+                          
                                     
 
-""")
+"""))
+     
      if(choice1==1):
            deposit=int(input('enter the amount'))
-           new_customer["balance"]+=deposit
+           print(deposit)
+           new_customer["balance"]=new_customer["balance"]+deposit
+           print(balance)
      elif(choice1==2):
            withdraw=int(input("enter the amount"))
            if new_customer["balance"]<withdraw:
                   print("not sufficent amount")       
            else:
-                  new_customer["balance"]-=deposit
+                  new_customer["balance"]=new_customer["balance"]-deposit
+
      
 
                     
