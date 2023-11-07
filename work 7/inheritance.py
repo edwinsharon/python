d5c15ed062fcbb62d0756af6c206ccfe7c5aa658
edwinsharon <edwinachayan@gmail.com>
2023-11-07 12:43:31 +0530

    done

---
+++ work 7/inheritance.py
@@ -0,0 +1,14 @@
+class Course:
+    def python(that):
+        print("hai")
+class College(Course):
+    def java(than):
+        print("basket")
+class School(College):
+    def cpp(tere):
+        print("volleyball")        
+
+w=School()
+w.python()
+w.java()
+w.cpp()
