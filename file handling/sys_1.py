6158adc0902b90083dc89f72596e620641d2dfb9
edwinsharon <edwinachayan@gmail.com>
2023-11-16 13:21:11 +0530

    done

---
+++ file handling/sys_1.py
@@ -0,0 +1,16 @@
+# r-read
+# a-append
+# w-write
+# x-create
+# t-text
+# # b-binary
+# f=open("doc.txt","w")
+# # f=open("doc.txt","rt")
+# # 
+# f.write("beautiful")
+# f.close()
+# p=open("doc.txt")
+# print(p.read())
+# 
+import os
+os.rmdir("hello")
